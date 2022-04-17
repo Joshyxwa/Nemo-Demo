3163cff8122915f004241446f57a7d802e807667
Joshua Wong <zhuoyijoshua@gmail.com>
2022-04-17 17:39:58 +0000

    .

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Nemo-Demo
  Demostration of Nvidia Nemo Application
+
+
+# Important Resources Links
+https://docs.nvidia.com/deeplearning/nemo/user-guide/docs/en/main/asr/intro.html
--- application/route.py
+++ application/route.py
@@ -1,8 +1,48 @@
+import nemo.collections.asr as nemo_asr
+import nemo.collections.nlp as nemo_nlp
+from crypt import methods
 from application import app
-from flask import render_template
+from flask import redirect, render_template, request, flash
+import onnx
+import librosa
+import soundfile as sf
+import os
+import json
 
-import pyaudio
-import wave
+app.config["AUDIO_UPLOADS"] = "application/static/audio"
+app.config["JSON_DATA"] = "application/static/json"
+app.config["MODEL_FILES"] = "notebook"
+app.jinja_env.globals.update(config=app.config)
+
+# Model Functions
+def eng_asr_model(filename):
+
+    resampled_audio, sample_rate = librosa.load(os.path.join(app.config["AUDIO_UPLOADS"], filename), sr=22050)
+    citrinet_model = nemo_asr.models.EncDecCTCModelBPE.restore_from(restore_path=os.path.join(app.config["MODEL_FILES"], "eng_citrinet.nemo"))
+    # citrinet_model = onnx.load(os.path.join(app.config["MODEL_FILES"], "citrinet.onnx"))
+    sf.write("preprocessed_audio.wav", resampled_audio, sample_rate)
+    transcribed_text = citrinet_model.transcribe(paths2audio_files=["preprocessed_audio.wav"], batch_size=4)
+    return transcribed_text
+
+def chi_asr_model(filename):
+
+    resampled_audio, sample_rate = librosa.load(os.path.join(app.config["AUDIO_UPLOADS"], filename), sr=22050)
+    citrinet_model = nemo_asr.models.EncDecCTCModel.restore_from(restore_path=os.path.join(app.config["MODEL_FILES"], "chi_citrinet.nemo"))
+    # citrinet_model = onnx.load(os.path.join(app.config["MODEL_FILES"], "citrinet.onnx"))
+    sf.write("preprocessed_audio.wav", resampled_audio, sample_rate)
+    transcribed_text = citrinet_model.transcribe(paths2audio_files=["preprocessed_audio.wav"], batch_size=4)
+    return transcribed_text
+
+def punc_model(text):
+    puncbert_model = nemo_nlp.models.PunctuationCapitalizationModel.restore_from(restore_path=os.path.join(app.config["MODEL_FILES"], "punc_bert.nemo"))
+    transformed_text = puncbert_model.add_punctuation_capitalization([i for i in text])
+    dict_texts = {  "transcribed_text": text,
+                    "punc_text": transformed_text
+                }
+    # json_texts = json.loads(str(dict_texts))
+    with open(os.path.join(app.config["JSON_DATA"], 'punc_text.json'), 'w') as f:
+        json.dump(dict_texts, f, ensure_ascii=False)
+    return transformed_text
 
 #Handles http://127.0.0.1:5000/
 @app.route('/') 
@@ -11,7 +51,44 @@ import wave
 def index_page(): 
     return render_template("index.html")
 
-@app.route('/record')
-def record_audio():
-    
+@app.route('/stop_record', methods=["POST", "GET"])
+def applyNemo_audio():
+    print('Hi')
+    if request.method == "POST":
+        print('Hi')
+        wav_file = request.data()
+        transcribed_text = eng_asr_model(wav_file)
+        print(transcribed_text)
+        punc_text = punc_model(transcribed_text)
+        print(punc_text)
+        flash(punc_text)
+
+    elif request.method == 'GET':
+        print('Hii')
     return render_template("index.html")
+
+@app.route('/upload_audiofile', methods=['GET', 'POST'])
+def upload_audiofile():
+    if request.method == "POST":
+        if request.files:
+            audio_file = request.files["audiofile"]
+            audio_file.save(os.path.join(app.config["AUDIO_UPLOADS"], audio_file.filename))
+            model_lang = request.form['selectlanguage']
+            print(model_lang)
+            if model_lang =='english':
+
+                transcribed_text = eng_asr_model(audio_file.filename)
+                print(transcribed_text)
+                punc_text = punc_model(transcribed_text)
+                print(punc_text)
+                flash(punc_text)
+
+            else:
+                transcribed_text = chi_asr_model(audio_file.filename)
+                print(transcribed_text)
+                punc_text = punc_model(transcribed_text)
+                print(punc_text)
+                flash(punc_text)
+
+            return redirect(request.url)
+    return render_template('index.html')
